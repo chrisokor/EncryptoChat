78b53bc350f0147d593cb848a82414bd10c7625c
Christus Okorochukwu <110692932+chrisokor@users.noreply.github.com>
2025-08-09 19:07:44 -0700

    feat: Add send and inbox API calls for chat client

--- chat_client.py
+++ chat_client.py
@@ -1,7 +1,8 @@
-from nacl.public import PrivateKey, Box
+from nacl.public import PrivateKey, Box, PublicKey
 import requests
 from utils.constants import API
 from utils.base_64_utils import bytes_to_base64_str, base64_str_to_bytes
+from nacl.utils import random as nacl_random
 
 
 class ChatClient:
@@ -19,16 +20,44 @@ class ChatClient:
             "public_key": bytes_to_base64_str(bytes(self.public_key))
         })
         r.raise_for_status()
-        print(f"{self.username} registered")
+        print(f"[{self.username}] registered")
 
     # retrieve a peer's public key and create a Box (runs D-H inside)
     def handshake_with(self, peer: str):
         r = requests.get(f"{API}/users/{peer}")
         r.raise_for_status()
-        peer_public_key = base64_str_to_bytes(r.json()["public_key"])
+        peer_public_key = PublicKey(base64_str_to_bytes(r.json()["public_key"]))
         self.shared_boxes[peer] = Box(self.secret_key, peer_public_key)
-        print(f"{self.username} session ready with {peer}.")
+        print(f"[{self.username}] session ready with [{peer}].")
 
-    
+    # encrypt and send messages
+    def send_message(self, to: str, plaintext: str):
+        box = self.shared_boxes[to]
+        nonce = nacl_random(Box.NONCE_SIZE)
+        ciphertext = box.encrypt(plaintext.encode(), nonce)
+        request = requests.post(f"{API}/send", json={
+            "to": to,
+            "frm": self.username,
+            "ciphertext": bytes_to_base64_str(ciphertext)
+        })
+        request.raise_for_status()
+        print(f"[{self.username}] -> [{to}] sent.")
+
+
+    # retrieve inbox and decrypt messages
+    def receive_all(self):
+        request = requests.get(f"{API}/inbox/{self.username}")
+        request.raise_for_status()
+        messages = request.json()["inbox"]
+
+        for message in messages:
+            frm = message["from"]
+            box = self.shared_boxes.get(frm)
+
+            if not box:
+                self.handshake_with(frm)
+                box = self.shared_boxes[frm]
+            plaintext = box.decrypt(base64_str_to_bytes(message["ciphertext"])).decode()
+            print(f"[{self.username}] <- [{frm}]: [{plaintext}]")
 
 
