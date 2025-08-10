0d241dc22c2b7f0f6b095277435cade4e7e23b8b
Christus Okorochukwu <110692932+chrisokor@users.noreply.github.com>
2025-08-09 17:50:50 -0700

    feat: Create client to facilitate chat server access

---
+++ chat_client.py
@@ -0,0 +1,34 @@
+from nacl.public import PrivateKey, Box
+import requests
+from utils.constants import API
+from utils.base_64_utils import bytes_to_base64_str, base64_str_to_bytes
+
+
+class ChatClient:
+
+    def __init__(self, username: str):
+        self.username = username
+        self.secret_key = PrivateKey.generate()
+        self.public_key = self.secret_key.public_key
+        self.shared_boxes = {}
+
+    # register a user - generate their public key
+    def register(self):
+        r = requests.post(f"{API}/register", json={
+            "username": self.username,
+            "public_key": bytes_to_base64_str(bytes(self.public_key))
+        })
+        r.raise_for_status()
+        print(f"{self.username} registered")
+
+    # retrieve a peer's public key and create a Box (runs D-H inside)
+    def handshake_with(self, peer: str):
+        r = requests.get(f"{API}/users/{peer}")
+        r.raise_for_status()
+        peer_public_key = base64_str_to_bytes(r.json()["public_key"])
+        self.shared_boxes[peer] = Box(self.secret_key, peer_public_key)
+        print(f"{self.username} session ready with {peer}.")
+
+    
+
+
