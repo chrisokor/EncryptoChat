15503c9e6a75d2c51e1b2683aa4f2339cb61acbd
Christus Okorochukwu <110692932+chrisokor@users.noreply.github.com>
2025-12-21 22:22:04 -0500

    feat: Add models for PostgreSQL database tables

---
+++ models/database_models.py
@@ -0,0 +1,38 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, DateTime, Boolean, ForeignKey
+from sqlalchemy.sql import func
+from datetime import datetime
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    username = Column(String, primary_key=True, index=False)
+    public_key = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Prekey(Base):
+    __tablename__ = "prekeys"
+
+    id = Column(String, primary_key=True)
+    username = Column(String, ForeignKey("users.username", ondelete="CASCADE"), nullable=False, index=True)
+    key = Column(String, nullable=False)
+    used = Column(Boolean, default=False, nullable=False)
+    used_at = Column(DateTime(timezone=True), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    to_user = Column(String, ForeignKey("users.username", ondelete="CASCADE"), nullable=False, index=True)
+    from_user = Column(String, nullable=False)
+    ciphertext = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+
