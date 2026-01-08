b5137e1ef2bb3717ac0504a6c94c11f93ae43c6c
Christus Okorochukwu <110692932+chrisokor@users.noreply.github.com>
2026-01-08 16:00:43 -0500

    feat: Test user data retrieval and error handling

---
+++ test/integration/test_get_user.py
@@ -0,0 +1,36 @@
+from nacl.public import PrivateKey
+from utils.base_64_utils import bytes_to_base64_str
+
+
+class TestGetUser:
+
+    def test_get_existing_user_returns_user_data(self, client):
+        # create and register a user
+        user_secret_key = PrivateKey.generate()
+        user_public_key = user_secret_key.public_key
+        username = "Alice"
+
+        client.post("/register", json={
+            "username": username,
+            "public_key": bytes_to_base64_str(bytes(user_public_key))
+        })
+
+        # retrieve user via GET request
+        response = client.get(f"/users/{username}")
+
+
+        # assert proper user data is returned in response
+        assert response.status_code == 200
+        assert response.json()["username"] == username
+        assert response.json()["public_key"] == bytes_to_base64_str(bytes(user_public_key))
+
+
+    def test_get_nonexistent_user_returns_404(self, client):
+        # attempt to retrieve nonexistent user via GET request
+        nonexisting_username = "Bob"
+
+        response = client.get(f"/users/{nonexisting_username}")
+
+        # assert response status code is 404 and returns "User not found."
+        assert response.status_code == 404
+        assert response.json()["detail"] == "User not found."
